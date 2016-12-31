43f80914353539b8d717c0af70b1bb615424f502
PuyoPuyo <SpamFishTS@gmail.com>
2016-12-31 00:50:26 +0000

    Add files via upload

--- window.py
+++ window.py
@@ -1,2 +1,27 @@
+
 import sys
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
+
+class Window(QtGui.QMainWindow):
+	
+	def __init__(self):
+		super(Window, self).__init__()
+		self.setGeometry(100, 100, 500, 500)
+		self.setWindowTitle("PCAP-Attack-Analyzor")
+		self.setWindowIcon(QtGui.QIcon('Audit.png'))
+		self.ExecButton()
+		self.show()
+	def ExecButton(self):
+		btn = QtGui.QPushButton("Execute", self)
+		btn.resize(btn.sizeHint())
+		btn.move(200,100)
+	def MenuBar(self):
+		
+
+
+def run():
+	app = QtGui.QApplication(sys.argv)
+	GUI = Window()
+	sys.exit(app.exec_())
+
+run()
