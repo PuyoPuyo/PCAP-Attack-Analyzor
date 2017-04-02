df01b67f673204e68d0f8ffb1d62e7603b524824
PuyoPuyo <SpamFishTS@gmail.com>
2017-04-02 17:00:19 +0300

    Add files via upload

---
+++ a.php
@@ -0,0 +1,13 @@
+<?php
+if(empty($_FILES['pcapfile']['name'])){ // $_FILES An associative array of items uploaded to the current script via the HTTP POST method. 
+	echo "Error: No file picked for uploading";
+	exit();
+}
+
+	$fileName = $_FILES['pcapfile']["name"];
+	$fileTmpLoc = $_FILES['pcapfile']["tmp_name"]; // File in the PHP tmp folder
+	$fileType = $_FILES['pcapfile']["type"]; // What type of file is uploaded
+	$fileSize = $_FILES['pcapfile']["size"]; // Size of the file
+
+	echo $_FILES['pcapfile']['data'];
+?>
